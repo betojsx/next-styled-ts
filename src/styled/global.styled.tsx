00b164c71c95b748006a33aab54fefb5c3ac09fc
Beto <beto@minify.com.br>
2020-03-30 01:32:09 -0300

    Add fonts

---
+++ src/styled/global.styled.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle` 
+	@import url('/static/fonts/stylesheet.css');
+
+	/* DEFAULTS */
+	body {
+		font-family: 'Knockout-Regular-Texto', sans-serif;
+	}
+
+	/* CSS RESET */
+	html {
+		box-sizing: border-box;
+		font-size: 16px;
+	}
+
+	*,
+	*:before,
+	*:after {
+		box-sizing: inherit;
+	}
+
+	body,
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6,
+	p,
+	ol,
+	ul {
+		margin: 0;
+		padding: 0;
+		font-weight: normal;
+	}
+
+	img {
+		max-width: 100%;
+		height: auto;
+	}
+
+`;
+
+export default GlobalStyle;
--- src/styled/theme.tsx
+++ src/styled/theme.tsx
@@ -2,7 +2,10 @@ const theme = {
   yellow: '#ffc100',
   brown: '#774032',
   red: '#ed1c24',
-  blue: '#0072bc'
+  blue: '#0072bc',
+  white: '#fff',
+  titleFont: 'BlockBerthold',
+  textFont: 'Knockout-Regular-Texto'
 };
 
 export default theme;
